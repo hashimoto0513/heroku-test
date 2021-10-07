db24d37ee0464ac15e3ca8c34484036ce0c2f430
hashimoto0513 <Yutaka0513>
2021-10-07 15:52:43 +0900

    edit変更

--- templates/Cards/edit.php
+++ templates/Cards/edit.php
@@ -8,33 +8,36 @@
     <aside class="column">
         <div class="side-nav">
             <h4 class="heading"><? __('Actions') ?></h4>
-            <?= $this->Form->postLink(
-                __('Delete'),
-                ['action' => 'delete', $card->id],
-                ['confirm' => __('Are you sure you want to delete # {0}?', $card->id), 'class' => 'side-nav-item']
-            ) ?>
-            <?= $this->Html->link(__('List Cards'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+            <?= $this->Html->link(__('カードリスト'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
         </div>
     </aside>
-    <div class="column-responsive column-80">
-        <div class="cards form content">
+    <div class="edit-column-responsive column-80">
+        <div class="cards form content edit-wrapper">
             <?= $this->Form->create($card) ?>
             <fieldset>
                 <legend><?= __('Edit Card') ?></legend>
                 <?php
-                    echo $this->Form->label('Rarity');
+                    echo $this->Form->label('レアリティ');
                     echo $this->Form->select('rarity_id',$rarities,['label' => 'Number']);
-                    echo $this->Form->label('version');
+                    echo $this->Form->label('収録弾');
                     echo $this->Form->select('version_id',$names);
-                    echo $this->Form->select('version_id',$versions);
-                    echo $this->Form->control('CardNumber',['label' => 'Number']);
-                    echo $this->Form->control('CardName');
+                    echo $this->Form->select('version_id',$versions,['class' => 'eidt_short_ver']);
+                    echo $this->Form->control('CardNumber',['label' => 'カードNo.']);
+                    echo $this->Form->control('CardName',['label' => 'カード名']);
+                    echo $this->Form->label('色');
                     echo $this->Form->select('color',$options);
-                    echo $this->Form->control('cost');
+                    echo $this->Form->control('cost',['label' => 'コスト','class' => 'edit_cost']);
                 ?>
             </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
-            <?= $this->Form->end() ?>
+            <div class="btn-wrapper">
+                <?= $this->Form->button('変更',['class' => 'edit_button']); ?>
+                <?= $this->Form->end() ?>
+                <?= $this->Form->postLink(
+                    __('削除'),
+                    ['action' => 'delete', $card->id],
+                    ['confirm' => __('Are you sure you want to delete # {0}?', $card->id), 'class' => 'delete-btn']
+                ) ?>
+            </div>
         </div>
     </div>
 </div>
