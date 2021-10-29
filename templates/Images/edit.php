2c6e8d1c136ba8c26208eed08efbbf4f26d7f851
hashimoto0513 <Yutaka0513>
2021-10-29 10:36:43 +0900

    img view変更2

--- templates/Images/edit.php
+++ templates/Images/edit.php
@@ -4,30 +4,33 @@
  * @var \App\Model\Entity\Image $image
  */
 ?>
+<?php  $this->Html->css('cake', ['block' => true]); ?>
 <div class="row">
     <aside class="column">
         <div class="side-nav">
             <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Form->postLink(
-                __('Delete'),
-                ['action' => 'delete', $image->id],
-                ['confirm' => __('Are you sure you want to delete # {0}?', $image->id), 'class' => 'side-nav-item']
-            ) ?>
-            <?= $this->Html->link(__('List Images'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+            <?= $this->Html->link(__('イメージリスト'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
         </div>
     </aside>
-    <div class="column-responsive column-80">
-        <div class="images form content">
+    <div class="edit-column-responsive column-80">
+        <div class="images form content edit-wrapper">
         <?= $this->Form->create($image, ['type' => 'file']); ?>
             <fieldset>
                 <legend><?= __('Edit Image') ?></legend>
                 <?php
-                    echo $this->Form->file('img');
-                    echo $this->Form->control('image_name');
+                    echo $this->Form->control('イメージ名');
+                    echo $this->Form->file('img',['class' => 'img_button']);
                 ?>
             </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
-            <?= $this->Form->end() ?>
+            <div class="btn-wrapper">
+                <?= $this->Form->button('変更',['class' => 'edit_button']); ?>
+                <?= $this->Form->end() ?>
+                <?= $this->Form->postLink(
+                    '削除',
+                    ['action' => 'delete', $image->id],
+                    ['confirm' => __('Are you sure you want to delete # {0}?', $image->id), 'class' => 'delete-btn']
+                ) ?>
+            </div>
         </div>
     </div>
 </div>
